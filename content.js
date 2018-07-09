e797ba66040585b78e3f799f0f518fb3ff822acb
Loui Zibdawi <loui.zibdawi@esentire.com>
2018-07-09 16:59:44 -0400

    Added checkboxes

--- content.js
+++ content.js
@@ -1,8 +1,4 @@
 var envisionWindow = document.getElementById('GROUP_Grp_WSS_COURSE_SECTIONS')
-if (envisionWindow != null) {
-    myWindow.document.write(envisionWindow.innerHTML)
-}
-
 var summaryTable = envisionWindow.getElementsByTagName("table")[0]
 
 var headers = summaryTable.getElementsByTagName('tr')[1]
@@ -12,20 +8,25 @@ if (headers != null) {
     col.appendChild(text)
     headers.appendChild(col)
 
-    myWindow.document.write(headers.innerHTML)
 }
 
 var rows = summaryTable.getElementsByTagName('tr')
 
 var i;
+
 for(i = 2; i < rows.length; i++) {
     var col = document.createElement('td')
     var checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
-    col.appendChild(checkbox)
 
-    if(i % 2 == 1) {
-        col.style.backgroundColor = 'rgb(247, 247, 247)'
+    // //If course is empty
+    if(rows[i].innerHTML.includes("Closed")) {
+        col.appendChild(checkbox)
+    }
+    else {
+        if (i % 2 == 1) {
+            col.style.backgroundColor = 'rgb(247, 247, 247)'
+        }
     }
 
     rows[i].appendChild(col)
