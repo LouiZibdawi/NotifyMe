c8d1964e5e94b171ff23c7fcc79c47d11d58a4f5
Loui Zibdawi <loui.zibdawi@esentire.com>
2018-07-10 17:07:23 -0400

    Added popup

--- content.js
+++ content.js
@@ -1,34 +1,54 @@
 var envisionWindow = document.getElementById('GROUP_Grp_WSS_COURSE_SECTIONS')
 var summaryTable = envisionWindow.getElementsByTagName("table")[0]
-
-var headers = summaryTable.getElementsByTagName('tr')[1]
-if (headers != null) {
-    var col = document.createElement('th')
-    var text = document.createTextNode('Notify me when available')
-    col.appendChild(text)
-    headers.appendChild(col)
-
-}
-
 var rows = summaryTable.getElementsByTagName('tr')
 
-var i;
+var i = getStartingIndex(rows)
+addToHeaders(i, summaryTable)
+addToRows(i+1, rows)
 
-for(i = 2; i < rows.length; i++) {
-    var col = document.createElement('td')
-    var checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-
-    // //If course is empty
-    if(rows[i].innerHTML.includes("Closed")) {
-        col.appendChild(checkbox)
+function getStartingIndex(rows){
+    //headers in the first row
+    if(rows[0].getElementsByTagName('th').length > 1) {
+        //headers at 1st row
+        return 0
     }
     else {
-        if (i % 2 == 1) {
-            col.style.backgroundColor = 'rgb(247, 247, 247)'
-        }
+        //headers at 2nd row
+        return 1
     }
-
-    rows[i].appendChild(col)
-
 }
+
+function addToHeaders(headerIndex, summaryTable) {
+    var headers = summaryTable.getElementsByTagName('tr')[i]
+    if (headers != null) {
+        var col = document.createElement('th')
+        var text = document.createTextNode('Notify me when available')
+        col.appendChild(text)
+        headers.appendChild(col)
+    }
+}
+
+function addToRows(startIndex, rows) {
+    //Move to first row of info after headers
+    for(startIndex; startIndex < rows.length; startIndex++) {
+        var col = document.createElement('td')
+        var checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+
+        // //If course is empty
+        if(rows[startIndex].innerHTML.includes("Closed")) {
+            col.appendChild(checkbox)
+        }
+        else {
+            if (i % 2 == 1) {
+                col.style.backgroundColor = 'rgb(247, 247, 247)'
+            }
+        }
+
+        rows[startIndex].appendChild(col)
+
+    }
+}
+
+
+var submit = document.getElementsByClassName('shortButton');
--- popup.js
+++ popup.js
@@ -1,11 +1,43 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+var test = document.getElementById('test')
+test.innerHTML = "hi"
+
+var userEmail;
+checkForEmail()
+
+function checkForEmail() {
+    chrome.storage.sync.get(['email'], function(result) {
+
+        if(!chrome.runtime.lastError) {
+            console.log('Value currently is ' + result.key)
+            userEmail = result.key
+        }
+        console.log('No value currently set!')
+    })
+
+    if(userEmail != null) {
+        var newStr = "Emails will be sent to: <br>" + result.key;
+
+        document.getElementById('enterEmail').innerHTML = newStr
+        document.getElementById('emailBox').style.visibility = "hidden"
+    }
+}
+
+function setEmail() {
+    
+    var test = document.getElementById('test')
+    test.innerHTML = "hi"
+
+    var email = document.getElementById('emailBox').value
+
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.storage.sync.set[{'email': email}], function(result) {
+            console.log('Value currently is ' + email);
+        });
+  });
+
+})
